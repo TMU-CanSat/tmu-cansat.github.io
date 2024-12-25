1e8eddbb6eb8e1685329837e21b5519812d2b44f
Ahmad El-Gohary <116977902+ahmadlgohary@users.noreply.github.com>
2024-12-24 23:00:13 -0500

    updated website- still need to do a few more pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-document.querySelector(".banner__close").addEventListener("click", function () {
-	this.closest(".banner").style.display = "none";
-});
-
 (function($) {
 
 	var	$window = $(window),
